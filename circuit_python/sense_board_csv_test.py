775132fa6ac194d22f58f249c545697ff9554b22
jens <jens@lea.io>
2021-07-27 17:37:41 +0200

    Improved SD card write version. Testing IRL now

--- circuit_python/sense_board_csv_test.py
+++ circuit_python/sense_board_csv_test.py
@@ -2,6 +2,9 @@
 import time
 import array
 import math
+import adafruit_sdcard
+import storage
+import busio
 import board
 import audiobusio
 import adafruit_apds9960.apds9960
@@ -18,6 +21,7 @@ from analogio import AnalogIn
 led = digitalio.DigitalInOut(board.RED_LED)
 led.switch_to_output()
 
+# Battery
 vbat_voltage = AnalogIn(board.VOLTAGE_MONITOR)
 
 
@@ -25,10 +29,21 @@ def get_voltage(pin):
     return (pin.value * 3.3) / 65536 * 2
 
 
+# SD Card
+SD_CS_PIN = board.D10  # Pin 10 for Feather Boards
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+sd_cs = digitalio.DigitalInOut(SD_CS_PIN)
+sdcard = adafruit_sdcard.SDCard(spi, sd_cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")  # Mount the SD Card under /sd
+
+
+# Boot switch
 switch = digitalio.DigitalInOut(board.SWITCH)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
+# Sensors
 i2c = board.I2C()
 
 apds9960 = adafruit_apds9960.apds9960.APDS9960(i2c)
@@ -36,13 +51,9 @@ bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 lis3mdl = adafruit_lis3mdl.LIS3MDL(i2c)
 
 lsm6ds33 = adafruit_lsm6ds.lsm6ds33.LSM6DS33(i2c)
-lsm6ds33.accelerometer_range = AccelRange.RANGE_2G
-lsm6ds33.accelerometer_data_rate = Rate.RATE_26_HZ
-# no gyro used for step detection
-lsm6ds33.gyro_data_rate = Rate.RATE_SHUTDOWN
-lsm6ds33.pedometer_enable = True
 
 sht31d = adafruit_sht31d.SHT31D(i2c)
+
 microphone = audiobusio.PDMIn(
     board.MICROPHONE_CLOCK, board.MICROPHONE_DATA, sample_rate=16000, bit_depth=16
 )
@@ -63,6 +74,7 @@ def normalized_rms(values):
     )
 
 
+# Configure the Sensors
 apds9960.enable_proximity = True
 apds9960.enable_color = True
 
@@ -70,43 +82,74 @@ apds9960.enable_color = True
 # for accurate altitude reading.
 bmp280.sea_level_pressure = 1013.25
 
+lsm6ds33.accelerometer_range = AccelRange.RANGE_2G
+lsm6ds33.accelerometer_data_rate = Rate.RATE_26_HZ
+# no gyro used for step detection
+lsm6ds33.gyro_data_rate = Rate.RATE_SHUTDOWN
+lsm6ds33.pedometer_enable = True
+
+
+def reset_pedometer():
+    lsm6ds33.pedometer_enable = False
+    lsm6ds33.pedometer_enable = True
+
+
+def timestamp():
+    t = rtc.datetime
+    return "{}-{}-{}T{}:{}:{}".format(
+        t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec
+    )
+
+
+LOG_FILE = "/sd/data.csv"
+
+
+def logfile():
+    return open(LOG_FILE, "at")
+
+
+# Prepare log file
+with logfile() as file:
+    file.write("# New record {}\r\n".format(timestamp()))
+    file.flush()
+
 try:
     while True:
         print(switch.value)
 
+        # Turn off the onbaord neopixel
         neo[0] = (0, 0, 0)
 
-        t = rtc.datetime
-        now = "{}-{}-{}T{}:{}:{}".format(
-            t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec
-        )
+        now = timestamp()
 
         battery_voltage = get_voltage(vbat_voltage)
 
         samples = array.array("H", [0] * 160)
         microphone.record(samples, len(samples))
-        print("\nTime", now)
-        print("Feather Sense Sensor Demo")
-        print("---------------------------------------------")
-        print("Proximity:", apds9960.proximity)
-        print("Red: {}, Green: {}, Blue: {}, Clear: {}".format(*apds9960.color_data))
-        print("Temperature: {:.1f} C".format(bmp280.temperature))
-        print("Barometric pressure:", bmp280.pressure)
-        print("Altitude: {:.1f} m".format(bmp280.altitude))
-        print("Magnetic: {:.3f} {:.3f} {:.3f} uTesla".format(*lis3mdl.magnetic))
-        print("Acceleration: {:.2f} {:.2f} {:.2f} m/s^2".format(*lsm6ds33.acceleration))
-        print("Gyro: {:.2f} {:.2f} {:.2f} dps".format(*lsm6ds33.gyro))
-        print("Humidity: {:.1f} %".format(sht31d.relative_humidity))
-        print("Sound level:", normalized_rms(samples))
-        print("Batt: {}".format(battery_voltage))
-        print("Steps: {}".format(lsm6ds33.pedometer_steps))
         
+        # print("\nTime", now)
+        # print("Feather Sense Sensor Demo")
+        # print("---------------------------------------------")
+        # print("Proximity:", apds9960.proximity)
+        # print("Red: {}, Green: {}, Blue: {}, Clear: {}".format(*apds9960.color_data))
+        # print("Temperature: {:.1f} C".format(bmp280.temperature))
+        # print("Barometric pressure:", bmp280.pressure)
+        # print("Altitude: {:.1f} m".format(bmp280.altitude))
+        # print("Magnetic: {:.3f} {:.3f} {:.3f} uTesla".format(*lis3mdl.magnetic))
+        # print("Acceleration: {:.2f} {:.2f} {:.2f} m/s^2".format(*lsm6ds33.acceleration))
+        # print("Gyro: {:.2f} {:.2f} {:.2f} dps".format(*lsm6ds33.gyro))
+        # print("Humidity: {:.1f} %".format(sht31d.relative_humidity))
+        # print("Sound level:", normalized_rms(samples))
+        # print("Batt: {}".format(battery_voltage))
+        # print("Steps: {}".format(lsm6ds33.pedometer_steps))
+
         data = []
-        
+
         data.append(now)
         data.append(apds9960.proximity)
         data.append("{},{},{},{}".format(*apds9960.color_data))
         data.append(bmp280.temperature)
+        data.append(bmp280.pressure)
         data.append(bmp280.altitude)
         data.append("{:.2f},{:.2f},{:.2f}".format(*lis3mdl.magnetic))
         data.append("{:.2f},{:.2f},{:.2f}".format(*lsm6ds33.acceleration))
@@ -115,14 +158,19 @@ try:
         data.append(normalized_rms(samples))
         data.append(battery_voltage)
         data.append(lsm6ds33.pedometer_steps)
-        
-        with open("data.csv", "at") as file:
+
+        reset_pedometer()
+
+        # Write the data capture to file
+        with logfile() as file:
             led.value = True
             file.write(",".join(str(e) for e in data) + "\r\n")
             file.flush()
             led.value = False
 
         time.sleep(1)
+
+# Trap the system in a blinking state when an error occured
 except OSError as e:
     delay = 0.5
     # 28 means out of space
